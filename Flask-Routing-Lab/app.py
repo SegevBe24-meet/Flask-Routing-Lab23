593c3020683400ded540b5b9d8302904b664fa64
SegevBe24-meet <139688996+SegevBe24-meet@users.noreply.github.com>
2023-07-18 18:37:16 +0300

    q

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -7,11 +7,17 @@ app = Flask(  # Create a flask app
     static_folder='static' 
 )
 
-@app.route('/homepage')
+@app.route('/')
 def homePage():
     return render_template("home.html")
 
+@app.route('/items')
+def product():
+    return render_template("product.html")
 
+@app.route('/cart')
+def cart():
+    return render_template("cart.html")
 
 if __name__ == "__main__": 
     app.run(debug=True)
